6da5a5eea6d386c217ee8a5f35a3961e4c99caf0
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-03-05 15:54:24 +0700

    Implement event channel in iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,12 +2,39 @@ import UIKit
 import Flutter
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+@objc class AppDelegate: FlutterAppDelegate, FlutterStreamHandler {
+    private var eventSink: FlutterEventSink?
+    private var timer: Timer?
+    private var counter = 0
+    
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        GeneratedPluginRegistrant.register(with: self)
+        
+        let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+        let eventChannel = FlutterEventChannel(name: "timerEventChannel", binaryMessenger: controller.binaryMessenger)
+        eventChannel.setStreamHandler(self)
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: "increaseCounter", userInfo: nil, repeats: true)
+        
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+    
+    @objc func increaseCounter() {
+        counter += 1
+        eventSink?(counter)
+    }
+    
+    public func onListen(withArguments arguments: Any?, eventSink: @escaping FlutterEventSink) -> FlutterError? {
+        self.eventSink = eventSink
+        return nil
+    }
+    
+    public func onCancel(withArguments arguments: Any?) -> FlutterError? {
+        timer?.invalidate()
+        eventSink = nil
+        return nil
+    }
 }
